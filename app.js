5ea43b32049616f658d0a5f0af7cb42c282d5ad9
sahin.maral <sahin.maral@hotmail.com>
2022-07-06 14:19:56 +0300

    Added template engine, views folder

--- app.js
+++ app.js
@@ -2,8 +2,34 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// Template Engines
+app.set('view engine', 'ejs')
+
+// Middlewares
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
-  res.status(200).send('hello world');
+  res.status(200).render('index',{page_name:'index'});
+});
+
+app.get('/about', (req, res) => {
+  res.status(200).render('about',{page_name:'about'});
+});
+
+app.get('/contact', (req, res) => {
+  res.status(200).render('contact',{page_name:'contact'});
+});
+
+app.get('/courses', (req, res) => {
+  res.status(200).render('courses',{page_name:'courses'});
+});
+
+app.get('/login', (req, res) => {
+  res.status(200).render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.status(200).render('register');
 });
 
 app.listen(port, () => {
